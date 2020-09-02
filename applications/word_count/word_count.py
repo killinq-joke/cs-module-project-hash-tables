8deeb65a582e02a943ffa94eddac4d2649c41ec1
zakaria touzri <zakadmaster@gmail.com>
2020-09-02 21:54:11 +0200

    ADDED: word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,23 @@
 def word_count(s):
     # Your code here
-
+    res = {}
+    for i in ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']:
+        s = s.replace(f"{i}", '')
+    for i in ['\r', '\t', '\n']:
+        s = s.replace(f"{i}", " ")
+    s = s.lower()
+    s = s.split(" ")
+    for i in s:
+        if i != "":
+            res[f"{i}"] = s.count(i)
+    return res
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
