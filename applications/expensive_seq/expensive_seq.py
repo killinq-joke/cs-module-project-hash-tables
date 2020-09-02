3e79ba01f285be5d4ee0a5d742af7f7c19bc3773
zakaria touzri <zakadmaster@gmail.com>
2020-09-02 22:22:36 +0200

    ADDED: expseq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,17 +1,16 @@
 # Your code here
 from hashtable import HashTable
-cache = HashTable(8)
+cache = {}
+
 
 def expensive_seq(x, y, z):
     # Your code here
-    if cache.get(f"{x}, {y}, {z}"):
-        return cache.get(f"{x}, {y}, {z}")
-    else:
-        if x <= 0: y + z
-        if x >  0: expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
-        cache.put(f"{x}, {y}, {z}", expensive_seq(x-1,y+1,z))
-    
-    return x + y + z
+    global cache
+    if x <= 0:
+        return y + z
+    if (x, y, z) not in cache:
+        cache[(x, y, z)] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+    return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
