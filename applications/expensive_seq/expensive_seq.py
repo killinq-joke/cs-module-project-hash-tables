707e6a45b861616140436ce4c4272483db461fcc
zakaria touzri <zakadmaster@gmail.com>
2020-09-02 21:10:50 +0200

    DECRYTED: exps readme hint

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
 # Your code here
-
+from hashtable import HashTable
+cache = HashTable(8)
 
 def expensive_seq(x, y, z):
     # Your code here
-
+    if cache.get(f"{x}, {y}, {z}"):
+        return cache.get(f"{x}, {y}, {z}")
+    else:
+        if x <= 0: y + z
+        if x >  0: expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        cache.put(f"{x}, {y}, {z}", expensive_seq(x-1,y+1,z))
+    
+    return x + y + z
 
 
 if __name__ == "__main__":
